c9d94cfce6b96c7785cf21cb94f456cab155b949
Kevnjyp <kevin.jiaoyp@gmail.com>
2019-03-04 09:35:57 +0800

    refract wordset.insert
    长度从大到小，尾字母逆序

--- Wordlist/WordSet.cpp
+++ Wordlist/WordSet.cpp
@@ -12,8 +12,28 @@ void WordSet::append(Word w) {
 			suc = false;
 		}
 	}
-	if (suc)
-		this->set[w.head - 'a'].push_back(w);
+	if (suc) {
+		//this->set[w.head - 'a'].push_back(w);
+		if (this->set[w.head - 'a'].size() == 0)
+			this->set[w.head - 'a'].push_back(w);
+		else {
+			bool insert = false;
+			for (auto iter = this->set[w.head - 'a'].begin(); iter < this->set[w.head - 'a'].end(); iter++) {
+				if (iter->tail == w.tail && iter->length <= w.length) {
+					this->set[w.head - 'a'].insert(iter, 1, w);
+					insert = true;
+					break;
+				}
+				if (iter->tail < w.tail) {
+					this->set[w.head - 'a'].insert(iter, 1, w);
+					insert = true;
+					break;
+				}
+			}
+			if(!insert)
+				this->set[w.head - 'a'].push_back(w);
+		}
+	}
 }
 
 //Word WordSet::getWord(char head) {
